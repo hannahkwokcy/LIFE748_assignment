22d0b92d3dfb4bf949517ba74cf6fb413f1c634b
Hannah <kwokhannah30@gmail.com>
2026-04-15 23:06:11 +0100

    Final ammendments

--- part3/sequence_extraction.py
+++ part3/sequence_extraction.py
@@ -7,14 +7,14 @@ target_degs = ["nikA", "arsC", "stpA", "lrp", "argR"]
 
 # 2. Define pairs of (TSV, FAA) for each pipeline
 pipelines = {
-    "Flye_Prokka": {"tsv": "/home/hannah/LIFE748asm/part1/annotations/prokka_flye.tsv", 
-                    "faa": "/home/hannah/LIFE748asm/part1/annotations/prokka_flye.faa"},
-    "SPAdes_Prokka": {"tsv": "/home/hannah/LIFE748asm/part1/annotations/prokka_spades.tsv", 
-                      "faa": "/home/hannah/LIFE748asm/part1/annotations/prokka_spades.faa"},
-    "Flye_Bakta": {"tsv": "/home/hannah/LIFE748asm/part1/annotations/bakta_flye.tsv", 
-                   "faa": "/home/hannah/LIFE748asm/part1/annotations/bakta_flye.faa"},
-    "SPAdes_Bakta": {"tsv": "/home/hannah/LIFE748asm/part1/annotations/bakta_spades.tsv",
-                    "faa": "/home/hannah/LIFE748asm/part1/annotations/bakta_spades.faa"}
+    "Flye_Prokka": {"tsv": "/home/hannah/LIFE748asm/part1/annotation/annotations/prokka_flye.tsv", 
+                    "faa": "/home/hannah/LIFE748asm/part1/annotation/annotations/prokka_flye.faa"},
+    "SPAdes_Prokka": {"tsv": "/home/hannah/LIFE748asm/part1/annotation/annotations/prokka_spades.tsv", 
+                      "faa": "/home/hannah/LIFE748asm/part1/annotation/annotations/prokka_spades.faa"},
+    "Flye_Bakta": {"tsv": "/home/hannah/LIFE748asm/part1/annotation/annotations/bakta_flye.tsv", 
+                   "faa": "/home/hannah/LIFE748asm/part1/annotation/annotations/bakta_flye.faa"},
+    "SPAdes_Bakta": {"tsv": "/home/hannah/LIFE748asm/part1/annotation/annotations/bakta_spades.tsv",
+                    "faa": "/home/hannah/LIFE748asm/part1/annotation/annotations/bakta_spades.faa"}
 }
 
 extracted_records = []
